1609ea1fb8e1babd6314b5a50c2bd6a90bf5b8e9
Hannah McLaughlin <h@mcla.ug>
2026-09-22 00:37:43 +0100

    Update convert script and database schema
    Following #12.
    
    I will consider adding tables for categories in future.

--- convert.py
+++ convert.py
@@ -1,59 +1,46 @@
-import re
+import csv
 
 import sqlite3
 
 
 if __name__ == "__main__":
-    with open("mapdata_u.dat", "r") as f:
-        lines = f.readlines()
-    con = sqlite3.connect("lasid.db")
-    cur = con.cursor()
+    with open("maps.csv", "r") as f:
+        reader = csv.DictReader(filter(lambda row: row[0:2] != "/*", f))
+        con = sqlite3.connect("lasid.db")
+        cur = con.cursor()
 
-    n = 101
-    for i in range(0, len(lines) - 1, n):
-        m = lines[i:i+n]
+        processed_maps = []
+        for row in reader:
+            map_display_id = row["map"]
+            title = row["prompt"]
 
-        res = re.search(
-            r"<M ([0-9]*[a,b,c]?) (.*)>",
-            m[1],
-        )
-        display_id = res.group(1)
-        title = res.group(2)
+            if map_display_id not in processed_maps:
+                cur.execute(
+                    f"""
+                    insert into
+                        map (title, display_id)
+                        values (
+                            "{title}",
+                            "{map_display_id}"
+                        );
+                    """
+                )
+                processed_maps.append(map_display_id)
 
-        print(m[2])
-        categories = re.search(
-            r"<F (.*)>",
-            m[2],
-        ).groups()
-        categories = "; ".join(categories)
-
-
-        cur.execute(
-            f"""
-            insert into
-                map (title, categories, display_id)
-                values (
-                    "{title}",
-                    "{categories}",
-                    "{display_id}"
-                );
-            """
-        )
-
-        for p in m[3:]:
-            p = p.replace("|", "").rstrip().lstrip("0")
-            try:
-                survey_point_id, transcriptions = p.split(" ", 1)
-            except ValueError:
-                continue
+            survey_point_id = row["survey_point_id"]
+            transcription = row["transcription"]
+            note = row["note"]
+            category = row["category"]
 
             cur.execute(
                 f"""
                 insert into
-                    map_point (transcription, map_id, survey_point_id)
+                    map_point (transcription, note, category, map_id, survey_point_id)
                     values (
-                        "{transcriptions}",
-                        (select id from map where display_id = "{display_id}"),
+                        "{transcription}",
+                        "{note}",
+                        "{category}",
+                        (select id from map where display_id = "{map_display_id}"),
                         (select id
                             from survey_point
                             where display_id = "{survey_point_id}")
@@ -62,4 +49,4 @@ if __name__ == "__main__":
                 """
             )
 
-    con.commit()
+        con.commit()
